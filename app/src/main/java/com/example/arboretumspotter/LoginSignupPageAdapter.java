835e90c3c25ef79af777c8b813f0b5cd9bd943d6
noah-gregory <noah.gregory.fl@gmail.com>
2023-07-09 17:20:16 -0400

    Created login and signup page layouts

---
+++ app/src/main/java/com/example/arboretumspotter/LoginSignupPageAdapter.java
@@ -0,0 +1,34 @@
+package com.example.arboretumspotter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class LoginSignupPageAdapter extends FragmentStateAdapter
+{
+    private ArrayList<Fragment> fragmentList = new ArrayList<>();
+
+    public LoginSignupPageAdapter(FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        fragmentList.add(fragment);
+    }
+}
--- app/src/main/java/com/example/arboretumspotter/MainActivity.java
+++ app/src/main/java/com/example/arboretumspotter/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.arboretumspotter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    ViewPager2 myViewPager2;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ViewPager2 for swiping between login and signup fragments
+        ViewPager2 viewPager2 = findViewById(R.id.viewPager2);
+
+        // Page adapter
+        LoginSignupPageAdapter loginSignupPageAdapter = new LoginSignupPageAdapter(getSupportFragmentManager(), getLifecycle());
+
+        // Add fragments to LoginSignupPageAdapter
+        loginSignupPageAdapter.addFragment(new LoginFragment());
+        loginSignupPageAdapter.addFragment(new SignupFragment());
+        viewPager2.setAdapter(loginSignupPageAdapter);
     }
 }
